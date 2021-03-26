434024bbcac2523f027d53e5a81482618b0ce355
eandres5 <andresalquinga@hotmail.com>
2021-03-26 17:41:46 -0500

    26032021andres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rias-dev';
+
+  scroll1(seccion:HTMLElement){
+    seccion.scrollIntoView()
+  }
+
+  scroll2(seccion:HTMLElement){
+    seccion.scrollIntoView()
+  }
+  scroll3(seccion:HTMLElement){
+    seccion.scrollIntoView()
+  }
+  scroll4(seccion:HTMLElement){
+    seccion.scrollIntoView()
+  }
+  scroll5(seccion:HTMLElement){
+    seccion.scrollIntoView()
+  }
+  
+
 }
